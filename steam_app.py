ae75eedd2c81199db846a18a0c3556d5685e278d
parthwaghela04 <87163600+parthwaghela04@users.noreply.github.com>
2023-04-20 21:31:06 +0530

    Update steam_app.py

--- steam_app.py
+++ steam_app.py
@@ -1,7 +1,9 @@
 import streamlit 
+streamlit.title('Breakfast favroities')
 
-streamlit.title('This is my first app')
-streamlit.header ('Breakfast Menu' )
-streamlit. text ('Omega 3 & Blueberry Oatmeal')
-streamlit. text('Kale, Spinach & Rocket Smoothie') 
-streamlit.text ('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast favroities')
+streamlit.text ('🥗Breakfast Menu' )
+streamlit. text ('🥣Omega 3 & Blueberry Oatmeal')
+streamlit. text('🐔Kale, Spinach & Rocket Smoothie') 
+streamlit.text ('🥑🍞Hard-Boiled Free-Range Egg')
+   
