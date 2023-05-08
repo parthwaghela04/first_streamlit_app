cf178a2d5b0f8ece6a6c79f543921dbdc2022210
parthwaghela04 <87163600+parthwaghela04@users.noreply.github.com>
2023-05-08 13:28:19 +0530

    Update steam_app.py

--- steam_app.py
+++ steam_app.py
@@ -34,7 +34,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  + fruit_
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
+streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
